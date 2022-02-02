6b933712bbbb0a40bac9087db05ea7eb893caa77
María <maria_am16@hotmail.com>
2022-02-02 13:18:37 +0100

    Mensaje predeterminado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace proyecto_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensaje;
+
+            mensaje = "Hola" ;
+            Console.WriteLine (mensaje);
+
+            mensaje = "Estamos programando"; 
+            Console.Write (mensaje);
+            mensaje = Console.ReadLine(); 
+            Console.WriteLine ("se debería añadir esto" + mensaje); 
+            Console.ReadLine();
+        }
+    }
+}
